fa339a830af3b924c016cf226b8eabe83b6d80be
gu-does-git <gu.does.dev@gmail.come>
2022-06-02 19:23:07 -0300

    fixed the copas.loop() problem and got a first connection ver

--- lua/scriptsync/init.lua
+++ lua/scriptsync/init.lua
@@ -1,27 +1,75 @@
-local copas = require'copas'
+function set_timeout(timeout, callback)
+  local timer = vim.loop.new_timer()
+  local function ontimeout()
+    vim.loop.timer_stop(timer)
+    vim.loop.close(timer)
+    callback()
+  end
 
--- this callback is called, whenever a new client connects.
--- ws is a new websocket instance
-local echo_handler = function(ws)
-		print('received a new client')
-		ws:send('["SIUUUUUU É O LUVA DE CODEIRO MELHOR DO BRASIL"]') -- TODO: change this into a better message XD
+  vim.loop.timer_start(timer, timeout, 0, ontimeout)
+  return timer
 end
 
--- create a copas webserver and start listening
-local server = require'websocket'.server.copas.listen
-{
-  -- listen on port 8080
-  port = 1978,
-  -- the protocols field holds
-  --   key: protocol name
-  --   value: callback on new connection
-  protocols = {
-    -- this callback is called, whenever a new client connects.
-    -- ws is a new websocket instance
-    echo = echo_handler
-  },
-  default = echo_handler
-}
+--[[
+SERVER STUFF
+]]
+local copas = require 'copas'
 
--- use the copas loop
-copas.loop()
+-- Initial connection handler
+local initial_handler = function(ws)
+  SenderConnected = true;
+  LuaLineMessage = LuaLineTitle .. LuaLineSeparator .. "Connected"
+  ws:send('["Connected to NVIM ScriptSync WebSocket"]')
+end
+
+-- wss:// server to send stuff to snUtils
+local serverSender = require 'websocket'.server.copas.listen
+{
+  port = 1978,
+  protocols = {
+    echo = initial_handler
+  },
+  default = initial_handler
+}
+SenderConnected = false;
+
+--[[
+LUALINE STUFF
+]]
+LuaLineTitle = "snScriptSync"
+LuaLineSeparator = " > "
+LuaLineMessage = ""
+
+-- init LuaLineMessage
+function resetLuaLine()
+  LuaLineMessage = LuaLineTitle
+end
+
+local function luaLineController()
+  return LuaLineMessage
+end
+
+require('lualine').setup { sections = { lualine_a = { luaLineController } } }
+
+--[[
+EXPORT STUFF
+]]
+local run = function()
+  LuaLineMessage = LuaLineTitle .. LuaLineSeparator .. "Started"
+  set_timeout(1000, function()
+    resetLuaLine()
+  end)
+
+  -- i didn't use the copas loop
+  local timer = vim.loop.new_timer()
+  timer:start(0, 600, vim.schedule_wrap(function()
+    copas.step()
+  end))
+
+end
+
+
+--[[
+EXPORT STUFF
+]]
+return { run = run }
